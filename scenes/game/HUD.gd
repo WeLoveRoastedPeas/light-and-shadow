64ca0c10db4960c1d99d8150a7b9c794f28acfd2
David Nuon <davidnuongm@gmail.com>
2025-12-06 20:21:06 -0800

    added start of HUD

---
+++ scenes/game/HUD.gd
@@ -0,0 +1,26 @@
+extends MarginContainer
+
+@export var health : Node
+
+var is_alive: bool = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	health.connect("health_changed", _on_health_changed)
+	health.connect("died", _on_died)
+	
+func _on_died(_reason: String) -> void:
+	is_alive = false
+	$HP.text = "DEAD"
+	pass
+
+func update_health(health: int) -> void:
+	if is_alive:
+		$HP.text = "HP: " + str(health)
+
+func _on_health_changed(health: int) -> void:
+	update_health(health)
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
